cbc10fda182ca4fdfea467ff62b4f8d959585bf2
Yorcal <43748206+Yorcal@users.noreply.github.com>
2019-03-11 13:58:04 +0100

    Update code.py

--- code.py
+++ code.py
@@ -41,29 +41,29 @@ voisin = []
 
 """Fonction Lignes Horizontal"""
 
-def dessine_line_H(COULEUR_0):
+def dessine_line_H(couleur):
     debut = [0,0]
     fin = [WIDTH, 0]
     while debut[1]<HEIGHT:
         debut[1] += CASE
         fin[1] = debut[1]
-        pygame.draw.line(screen, COULEUR_0, debut, fin, 1)
+        pygame.draw.line(screen, couleur, debut, fin, 1)
     pygame.display.flip()
     
 """Fonction Lignes Verticale"""  
   
-def dessine_line_V(COULEUR_0):
+def dessine_line_V(couleur):
     debut = [0,0]
     fin = [0, HEIGHT]
     while debut[0]<WIDTH:
         debut[0] += CASE
         fin[0] = debut[0]
-        pygame.draw.line(screen, COULEUR_0, debut, fin, 1)
+        pygame.draw.line(screen, couleur, debut, fin, 1)
     pygame.display.flip()
 
-def dessine_quadrillage(COULEUR_0):
-    dessine_line_H(COULEUR_0)
-    dessine_line_V(COULEUR_0)
+def dessine_quadrillage(couleur):
+    dessine_line_H(couleur)
+    dessine_line_V(couleur)
 
 
 
@@ -79,4 +79,6 @@ pygame.display.init()
 #
 screen = pygame.display.set_mode(SIZE)
 pygame.display.flip()
+
+dessine_quadrillage(COULEUR_0)
 
