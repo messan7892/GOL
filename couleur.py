c1a698c8deae8126053380b286a441456ed177d0
messan7892 <messan7892@users.noreply.github.com>
2019-03-25 13:10:53 +0100

    Add files via upload

---
+++ couleur.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+
+# 16 couleurs de base en francais
+
+argent = (192,192,192)
+blanc = (255,255,255)
+bleu = (0,0,255)
+bleumarine = (0,0,128)
+citronvert = (0,255,0)
+cyan = (0,255,255)
+magenta = (255,0,255)
+gris = (128,128,128)
+jaune = (255,255,0)
+marron = (128,0,0)
+noir = (0,0,0)
+rouge = (255,0,0)
+sarcelle = (0,128,128)
+vert = (0,255,0)
+vertclair = (0,128,0)
+vertolive = (128,128,0)
+violet = (128,0,128)
+
+# 140 couleurs en anglais
+
+aliceblue = (240,248,255)
+antiquewhite = (250,235,215)
+aqua = (0,255,255)
+aquamarine = (127,255,212)
+azure = (240,255,255)
+beige = (245,245,220)
+bisque = (255,228,196)
+black = (0,0,0)
+blanchedalmond = (255,235,205)
+blue = (0,0,255)
+blueviolet = (138,43,226)
+brown = (165,42,42)
+burlywood = (210,35,887)
+cadetblue = (95,158,160)
+chartreuse = (127,255,0)
+#chocolate            D2691E
+coral = (255,7245,0)
+#cornflowerblue       6495ED
+cornsilk = (255,248,220)
+#crimson              DC143C
+cyan = (0,255,255)
+darkblue = (0,0,139)
+darkcyan = (0,139,139)
+#darkgoldenrod        B8860B
+darkgray = (169,169,169)
+darkgreen = (0,100,0)
+darkkhaki = (189,183,107)
+darkmagenta = (139,0,139)
+#darkolivegreen       556B2F
+darkorange = (255,140,0)
+#darkorchid           9932CC
+darkred = (139,0,0)
+darksalmon = (233,96122)
+darkseagreen = (143,188,143)
+#darkslateblue        483D8B
+darkslategray = (47,79,79)
+#darkturquoise        00CED1
+#darkviolet           9400D3
+#deeppink             FF1493
+deepskyblue = (0,191,255)
+dimgray = (105,105,105)
+#dodgerblue           1E90FF
+#firebrick            B22222
+floralwhite = (255,250,240)
+#forestgreen          228B22
+fuchsia = (255,0,255)
+gainsboro = (220,220,220)
+ghostwhite = (248,248,255)
+gold = (255,215,0)
+goldenrod = (218,165,20)
+gray = (128,128,128)
+green = (0,128,0)
+#greenyellow          ADFF2F
+honeydew = (240,255,240)
+hotpink = (255,105,180)
+indianred = (205,92,92)
+indigo = (75,0,82)
+ivory = (255,255,240)
+khaki = (240,230,140,)
+lavender = (230,230,250)
+lavenderblush = (255,240,245)
+#lawngreen            7CFC00
+lemonchiffon = (255,250,205)
+lightblue = (173,216,230)
+lightcoral = (240,128,128)
+lightcyan = (224,255,255)
+lightgoldenrodyellow = (250,250,210)
+#lightgreen           90EE90
+lightgrey = (211,211,211)
+lightpink = (255,182,193)
+lightsalmon = (255,160,122)
+#lightseagreen        20B2AA
+#lightskyblue         87CEFA
+#lightslategray       778899
+lightsteelblue = (176,196,222)
+lightyellow = (255,255,224)
+lime = (0,255,0)
+limegreen = (50,205,50)
+linen = (250,240,230)
+magenta = (255,0,255)
+maroon = (128,0,0)
+mediumaquamarine = (102,205,170)
+mediumblue = (0,0,205)
+#mediumorchid         BA55D3
+mediumpurple = (147,112,219)
+mediumseagreen = (60,179,71)
+#mediumslateblue      7B68EE
+mediumspringgren = (0,250,154)
+mediumturquoise = (48,209,204)
+mediumvioletred = (199,21,133)
+midnightblue = (25,25,112)
+mintcream = (245,255,250)
+mistyrose = (255,228,225)
+moccasin = (255,228,181)
+navajowhite = (255,222,173)
+navy = (0,0,128)
+#oldlace              FDF5E6
+olive = (128,128,0)
+#olivedrab            6B8E23
+orange = (255,165,0)
+orangered = (255,69,0)
+#orchid               DA70D6
+palegoldenrod = (238,225,38)
+#palegreen            98FB98
+paleturquoise = (175,238,238)
+#palevioletred        DB7093
+papayawhip = (255,239,213)
+peachpuff = (255,218,185)
+peru = (205,133,63)
+pink = (255,192,203)
+plum = (221,160,221)
+powderblue = (176,224,230)
+purple = (128,0,128)
+red = (255,0,0)
+#rosybrown            BC8F8F
+#royalblue            4169E1
+#saddlebrown          8B4513
+salmon = (250,128,72)
+sandybrown = (244,164,60)
+#seagreen             2E8B57
+seashell = (255,245,238)
+sienna = (160,82,45)
+silver = (192,192,192)
+#skyblue              87CEEB
+#slateblue            6A5ACD
+#slategray            708090
+snow = (255,250,250)
+springgreen = (0,255,127)
+#steelblue            4682B4
+tan = (210,180,140)
+teal = (0,128,128)
+#thistle              D8BFD8
+tomato = (255,6347)
+#turquoise            40E0D0
+#violetlight          EE82EE
+#wheat                F5DEB3
+white = (255,255,255)
+whitesmoke = (245,245,245)
+yellow = (255,255,0)
+#yellowgreen          9ACD32
